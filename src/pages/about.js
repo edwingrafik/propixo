db5925d01144c3223f5edd6f6ef67f4980996733
Edwin Gawron <edwingawron@gmail.com>
2020-05-26 13:51:31 +0200

    Colors

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import Layout from "../components/layout"
 const About = ({ data: { about } }) => (
   <Layout>
     <HelmetDatoCms seo={about.seoMetaTags} />
-    <div className='container font-sans'>
+    <div className='container text-text'>
       <div className='bg-red-900 w-8 h-8'></div>
       <div className='bg-red-800 w-16 h-16'></div>
       <div className='bg-red-700 w-24 h-24'></div>
@@ -16,48 +16,65 @@ const About = ({ data: { about } }) => (
       <div className='bg-red-400 w-104 h-104'></div>
       <div className='bg-red-300 w-168 h-168'></div>
       <div className='bg-red-200 w-272 h-272'></div>
-      <div className="h1-text">
+      <div className='h1-text'>
         Header 1<br />
         Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
         rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
         perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
         Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
-      <div className="h2-text">
+      <div className='h2-text'>
         Header 2<br />
         Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
         rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
         perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
         Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
-      <div className="h3-text">
+      <div className='h3-text'>
         Header 3<br />
         Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
         rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
         perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
         Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
-      <div className="h4-text">
+      <div className='h4-text'>
         Header 4<br />
         Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
         rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
         perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
         Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
-      <div className="body-text">
-        Text base<br />
+      <div className='body-text'>
+        Text base
+        <br />
         Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
         rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
         perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
         Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
-      <div className="small-text">
-        Text small<br />
+      <div className='small-text'>
+        Text small
+        <br />
         Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
         rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
         perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
         Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
+      <div className='flex flex-wrap -mx-16'>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-primary-lighter'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-primary'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-primary-darker'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-secondary-lighter'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-secondary'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-secondary-darker'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-text-lighter'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-text'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-text-darker'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-bg-lighter'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-bg'></div>
+        <div className='w-64 h-64 rounded-lg shadow-lg m-16 bg-bg-darker'></div>
+      </div>
+
       <h1 className='sheet__title'>{about.title}</h1>
       <p className='sheet__lead'>{about.subtitle}</p>
       <div className='sheet__gallery'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,35 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          lighter: '#b3bcf5',
+          default: '#5c6ac4',
+          darker: '#202e78',
+        },
+        secondary: {
+          lighter: '#71F4BD',
+          default: '#42F0A7',
+          darker: '#0BDA95',
+        },
+        bg: {
+          lighter: '#FAFAFA',
+          default: '#F5F5F5',
+          darker: '#F0F0F0',
+        },
+        text: {
+          lighter: '#8592AD',
+          default: '#3D475C',
+          darker: '#101119',
+        },
+      },
       letterSpacing: {
-        'tighter': '-.02em',
-        'tight': '-.01em',
-        'normal': '0em',
-        'wide': '.02em',
-        'wider': '.03em',
-        'widest': '.04em',
+        tighter: '-.02em',
+        tight: '-.01em',
+        normal: '0em',
+        wide: '.02em',
+        wider: '.03em',
+        widest: '.04em',
       },
       fontFamily: {
         sans: ['Raleway', ...defaultTheme.fontFamily.sans],
@@ -26,14 +48,14 @@ module.exports = {
         h1: ['4rem', '3.875rem'],
       },
       spacing: {
-        '8': '0.5rem',      // 8px
-        '16': '1rem',       // 16px
-        '24': '1.5rem',     // 24px
-        '40': '2.5rem',     // 40px
-        '64': '4rem',       // 64px
-        '104': '6.5rem',    // 104px
-        '168': '10.5rem',   // 168px
-        '272': '17rem',     // 272px
+        '8': '0.5rem', // 8px
+        '16': '1rem', // 16px
+        '24': '1.5rem', // 24px
+        '40': '2.5rem', // 40px
+        '64': '4rem', // 64px
+        '104': '6.5rem', // 104px
+        '168': '10.5rem', // 168px
+        '272': '17rem', // 272px
       },
     },
     screens: {
