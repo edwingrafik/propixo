4da2b6d2d1a6ccdeb03d16273c226e5730ea04a6
Edwin Gawron <edwingawron@gmail.com>
2020-05-26 12:19:57 +0200

    Tailwind config

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,24 +6,72 @@ import Layout from "../components/layout"
 
 const About = ({ data: { about } }) => (
   <Layout>
-    <article className="sheet">
-      <HelmetDatoCms seo={about.seoMetaTags} />
-      <div className="sheet__inner">
-        <h1 className="sheet__title">{about.title}</h1>
-        <p className="sheet__lead">{about.subtitle}</p>
-        <div className="sheet__gallery">
-          <Img fluid={about.photo.fluid} />
-        </div>
-        <div
-          className="sheet__body"
-          dangerouslySetInnerHTML={{
-            __html: about.bioNode.childMarkdownRemark.html,
-          }}
-        />
+    <HelmetDatoCms seo={about.seoMetaTags} />
+    <div className='container font-sans'>
+      <div className='bg-red-900 w-8 h-8'></div>
+      <div className='bg-red-800 w-16 h-16'></div>
+      <div className='bg-red-700 w-24 h-24'></div>
+      <div className='bg-red-600 w-40 h-40'></div>
+      <div className='bg-red-500 w-64 h-64'></div>
+      <div className='bg-red-400 w-104 h-104'></div>
+      <div className='bg-red-300 w-168 h-168'></div>
+      <div className='bg-red-200 w-272 h-272'></div>
+      <div className="text-h1">
+        Header 1<br />
+        Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
+        rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
+        perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
+        Tempore ipsum unde illo est rerum minima modi ea sunt.
       </div>
-    </article>
+      <div className="text-h2">
+        Header 2<br />
+        Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
+        rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
+        perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
+        Tempore ipsum unde illo est rerum minima modi ea sunt.
+      </div>
+      <div className="text-h3">
+        Header 3<br />
+        Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
+        rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
+        perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
+        Tempore ipsum unde illo est rerum minima modi ea sunt.
+      </div>
+      <div className="text-h4">
+        Header 4<br />
+        Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
+        rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
+        perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
+        Tempore ipsum unde illo est rerum minima modi ea sunt.
+      </div>
+      <div className="text-base">
+        Text base<br />
+        Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
+        rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
+        perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
+        Tempore ipsum unde illo est rerum minima modi ea sunt.
+      </div>
+      <div className="text-small">
+        Text small<br />
+        Eos placeat id impedit sint eum cumque nam in. Numquam quos quos autem
+        rerum ipsa voluptatem. Dolor reiciendis sit. Consequatur qui
+        perspiciatis non enim blanditiis. Autem recusandae ipsum sit vero.
+        Tempore ipsum unde illo est rerum minima modi ea sunt.
+      </div>
+      <h1 className='sheet__title'>{about.title}</h1>
+      <p className='sheet__lead'>{about.subtitle}</p>
+      <div className='sheet__gallery'>
+        <Img fluid={about.photo.fluid} />
+      </div>
+      <div
+        className='sheet__body'
+        dangerouslySetInnerHTML={{
+          __html: about.bioNode.childMarkdownRemark.html,
+        }}
+      />
+    </div>
   </Layout>
-)
+);
 
 export default About
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        label: ['0.875', '1rem'],
+        small: ['0.75rem', '1rem'],
+        base: ['1rem', '1.5rem'], // font-size: 16px, line-height: 24px
+        button: ['1rem', '1rem'],
+        h4: ['1.375rem', '1.75rem'],
+        h3: ['1.5rem', '2.125rem'],
+        h2: ['2.5rem', '2.75rem'],
+        h1: ['4rem', '3.875rem'],
+      },
+      spacing: {
+        '8': '0.5rem', // 8px
+        '16': '1rem', // 16px
+        '24': '1.5rem', // 24px
+        '40': '2.5rem', // 40px
+        '64': '4rem', // 64px
+        '104': '6.5rem', // 104px
+        '168': '10.5rem', // 168px
+        '272': '17rem', // 272px
+      },
+    },
+    screens: {
+      'sm': '768px',    // @media (min-width: 768px)
+      'md': '1024px',   // @media (min-width: 1024px)
+      'lg': '1440px',   // @media (min-width: 1440px)
+      'xl': '1920px',   // @media (min-width: 1920px)
+    },
+    container: {
+      center: true,
+      padding: {
+        default: '1.5rem',  // 24px
+        sm: '2.5rem',       // 40px
+        md: '4rem',         // 64px
+        lg: '10.5rem',      // 168px
+        xl: '17rem',        //272px
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
